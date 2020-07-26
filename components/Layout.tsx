a210dd1b59bd7bb3e8703736e89b2cffdfffa418
TokiyaHorikawa <ayikot4423@gmail.com>
2020-07-26 22:17:18 +0900

    feat: ヘッダーの仮設定

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,80 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core/";
+import MenuIcon from "@material-ui/icons/Menu";
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+
+
+const Layout = ({ children, title = "Home" }: Props) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Next Blog
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <nav>
+          <Link href="/">
+            <a>Home</a>
+          </Link>{" "}
+          |{" "}
+          <Link href="/about">
+            <a>About</a>
+          </Link>{" "}
+          |{" "}
+          <Link href="/users">
+            <a>Users List</a>
+          </Link>{" "}
+          | <a href="/api/users">Users API</a>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout
